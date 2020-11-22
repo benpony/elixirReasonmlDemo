e6940e384f1da939e196eaaaf964c118b3744354
ben pony <ben.pony.dev@gmail.com>
2020-11-22 17:14:49 +0200

    api / be impl init

---
+++ api/config/config.exs
@@ -0,0 +1,42 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :blog_api,
+  ecto_repos: [BlogApi.Repo],
+  generators: [binary_id: true]
+
+# Configures the endpoint
+config :blog_api, BlogApiWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "ksHYkHccY1Ndl9fKWFk+yM+LF2l0MaJ9RTLbc0YuFhGPpxl1GdUBfDiCaOG1duoK",
+  render_errors: [view: BlogApiWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: BlogApi.PubSub,
+  live_view: [signing_salt: "ivEBph9d"]
+
+# Configures Guardian
+config :blog_api, BlogApiWeb.Auth.GuardianSerializer,
+  issuer: "BlogApi",
+  secret_key: "MDLMflIpKod5YCnkdiY7C4E3ki2rgcAAMwfBl0+vyC5uqJNgoibfQmAh7J3uZWVK",
+  # optional
+  allowed_algos: ["HS256"],
+  ttl: {30, :days},
+  allowed_drift: 2000,
+  verify_issuer: true
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
---
+++ api/lib/blog_api_web/router.ex
@@ -0,0 +1,70 @@
+defmodule BlogApiWeb.Router do
+  use BlogApiWeb, :router
+
+  pipeline :api do
+    plug(BlogApiWeb.Auth.BearerAuthPipeline)
+    plug(BlogApiWeb.Plugs.GraphQLContext)
+  end
+
+  scope "/api" do
+    pipe_through([:api])
+
+    forward(
+      "/",
+      Absinthe.Plug,
+      schema: BlogApiWeb.Schema,
+      json_codec: Jason
+    )
+  end
+
+  # Enables LiveDashboard only for development
+  #
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through([:fetch_session, :protect_from_forgery])
+      live_dashboard("/dashboard", metrics: BlogApiWeb.Telemetry)
+    end
+  end
+
+  forward(
+    "/",
+    Absinthe.Plug.GraphiQL,
+    schema: BlogApiWeb.Schema,
+    json_codec: Jason
+  )
+end
+
+# defmodule BlogApiWeb.Router do
+#   use BlogApiWeb, :router
+
+#   pipeline :api do
+#     plug :accepts, ["json"]
+#   end
+
+#   scope "/api", BlogApiWeb do
+#     pipe_through :api
+#   end
+
+#   # Enables LiveDashboard only for development
+#   #
+#   # If you want to use the LiveDashboard in production, you should put
+#   # it behind authentication and allow only admins to access it.
+#   # If your application does not have an admins-only section yet,
+#   # you can use Plug.BasicAuth to set up some basic authentication
+#   # as long as you are also using SSL (which you should anyway).
+#   if Mix.env() in [:dev, :test] do
+#     import Phoenix.LiveDashboard.Router
+
+#     scope "/" do
+#       pipe_through [:fetch_session, :protect_from_forgery]
+#       live_dashboard "/dashboard", metrics: BlogApiWeb.Telemetry
+#     end
+#   end
+# end
